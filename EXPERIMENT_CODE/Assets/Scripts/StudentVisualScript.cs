19a64cc1f25bf49cf600a73c3a34fe3607f36269
Kaiser Lee <kaiserkaiserlee@gmail.com>
2025-08-25 19:02:44 -0700

    added audio

--- EXPERIMENT_CODE/Assets/Scripts/StudentVisualScript.cs
+++ EXPERIMENT_CODE/Assets/Scripts/StudentVisualScript.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class StudentVisualScript : MonoBehaviour
 {
     public Animator StudentAnimator;
+    public AudioSource StudentAudioSource;
 
     public void KillStudnet()
     {
         //make student dead
-        StudentAnimator.SetBool("StudentDead", true) ;
+        StudentAnimator.SetBool("StudentDead", true);
     }
 
     public void ShockStudent()
     {
         //shock student
         StudentAnimator.SetTrigger("enterShock");
+        StudentAudioSource.Play();
     }
 }
